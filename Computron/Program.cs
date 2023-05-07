ad2afedf404de3a9926d38f8f92cc5daef8219f6
Scott Falbo <scottfalboart@gmail.com>
2023-05-07 14:59:57 -0700

    moved api key to secrets

--- Computron/Program.cs
+++ Computron/Program.cs
@@ -2,13 +2,64 @@
 /// Computron, Open AI chat bot
 ///-----------------------------
 
+using Microsoft.Extensions.Configuration;
+
 namespace Computron
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static IOpenAIProxy _computron;
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            InitializeComputron();
+
+            Console.WriteLine("Computron");
+
+            await RunComputron();
+        }
+
+        private static async Task RunComputron()
+        {
+            Console.WriteLine("What would you like from Computron?");
+            var input = Console.ReadLine();
+
+            do
+            {
+                try
+                {
+                    var results = await _computron.Send(input);
+
+                    foreach (var item in results)
+                    {
+                        Console.WriteLine($"{item.Role}: {item.Content}");
+                    }
+
+                    Console.WriteLine("What else can Computron answer?");
+                    input = Console.ReadLine();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Computron has experienced an error");
+                    Console.WriteLine(ex.ToString());
+                    input = "goodbye";
+                }
+
+            }
+            while (input != "goodbye");
+        }
+
+        private static void InitializeComputron()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddUserSecrets(System.Reflection.Assembly.GetExecutingAssembly());
+
+            // Build the configuration
+            var configuration = builder.Build();
+            var apiKep = configuration["ApiKey"];
+            var organizationId = configuration["OrganizationId"];
+
+            _computron = new OpenAIProxy(apiKep, organizationId);
         }
     }
 }
