5965927174480ebf31a59296f1a4f285956f145a
Scott Falbo <scottfalboart@gmail.com>
2023-05-07 22:45:28 -0700

    added cartridge class

---
+++ Computron/Cartridges/Cartridge.cs
@@ -0,0 +1,29 @@
+﻿///-----------------------------
+/// Computron, Open AI chat bot
+///-----------------------------
+
+namespace Computron.Cartridges
+{
+    public abstract class Cartridge
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        private string Input { get; set; }
+
+        protected Cartridge(string name, string type, string input) 
+        {
+            Name = name;
+            Type = type;
+            Input = input;
+        }
+
+        public void ResetCartridge(string name, string type, string input)
+        {
+            Name = name;
+            Type = type;
+            Input = input;
+        }
+
+        public string ReadCartridge() => Input;
+    }
+}
---
+++ Computron/Cartridges/CartridgeType.cs
@@ -0,0 +1,11 @@
+﻿///-----------------------------
+/// Computron, Open AI chat bot
+///-----------------------------
+
+namespace Computron.Cartridges
+{
+    public class CartridgeType
+    {
+        public const string Personality = nameof(Personality);
+    }
+}
